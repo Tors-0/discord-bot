331d5549bc5dcfcdd40572fdeb3f16b4f0d52bf4
rae <raej0000@gmail.com>
2024-05-14 21:07:04 -0700

    wow such many features

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 // Require the necessary discord.js classes
 const fs = require('fs')
 const path = require('path')
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { ActivityType, Client, Collection, Events, GatewayIntentsBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]}
-);
+// calculate intents number at https://discord-intents-calculator.vercel.app
+const client = new Client({intents: 34307});
 
-client.commands = new Collection()
+let programStartTime = new Date(Date.now()).toLocaleString('sv-SE');
+
+const logFilePath = path.join(__dirname, `logs/discord-bot_${programStartTime}.log`);
+if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+	fs.mkdir(path.join(__dirname, 'logs'), err => {
+		if (err) {
+			console.error('Error creating \'logs\' directory:' + err)
+		} else {
+			console.log('Successfully created \'logs\' directory')
+		}
+	});
+} else {
+	console.log('logs directory already exists, continuing...');
+}
+
+console.originalLog = console.log;
+
+console.log = function(message) {
+	fs.appendFile(logFilePath, message + '\n', (err) => {
+		if (err) {
+			console.error('Error appending to log file:', err);
+		} else {
+			console.originalLog(message);
+		}
+	});
+}
+console.log(`now logging to ${logFilePath}`);
+
+client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -34,16 +61,36 @@ for (const folder of commandFolders) {
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	// flavor text for status
+	client.user.setActivity('with human souls', { type: ActivityType.Playing });
 });
-
-// when pinged in a reply, accuse the replied message of taking our position and delete the message with the ping
-client.on(Events.MessageCreate, async message => {
+let msgReplyPairs = new Map();
+msgReplyPairs.set('helldivers', 'omg isn\'t that like deep rock galactic but with bigger bugs?');
+msgReplyPairs.set('digital circus', 'im still sad about gummigoo =(');
+msgReplyPairs.set('outer wilds', 'HOLY SHIT I LOVE OUTER WILDS');
+// when pinged or in a reply, accuse the replied message of taking our position and delete the message with the ping
+client.on('messageCreate', async message => {
 	if (message.reference === null || message.reference === undefined) return;
-	let refMessage = await message.fetchReference();
-	if (refMessage.author.bot || message.author.bot) return;
-	await refMessage.reply('How dare you lay claim to my position??? I am the **true** Judge of Hell!')
-	await message.delete()
-	console.log('did the funny')
+
+	let originalContent = message.content;
+	let referredMessage = await message.fetchReference();
+	// stop the infinite loop
+	if (message.author.bot || referredMessage.author.bot) return;
+	let referredContent = referredMessage.content;
+
+	for (let [key, value] of msgReplyPairs.entries()) {
+		if (originalContent.includes(key)) {
+			if (Math.random() < 0.1) {
+				await message.reply(value);
+				return;
+			}
+		} else if (referredContent.includes(key)) {
+			if (Math.random() < 0.1) {
+				await referredMessage.reply(value);
+				return;
+			}
+		}
+	}
 })
 
 client.on(Events.InteractionCreate, async interaction => {
@@ -70,6 +117,3 @@ client.on(Events.InteractionCreate, async interaction => {
 
 // Log in to Discord with your client's token
 client.login(token);
-
-// flavor text for status
-//client.user.setActivity('with human souls');
