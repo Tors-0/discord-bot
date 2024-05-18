b2e7bfb6923a78e60a67bcbc6b6b140fdca931e1
rae <raej0000@gmail.com>
2024-05-18 00:18:14 -0700

    add new message replies

--- data/messageReplies.js
+++ data/messageReplies.js
@@ -1,35 +1,39 @@
 module.exports = {
     messageMap: new Map([
-        ['helldiver', [
+        [new RegExp('helldiver', 'i'), [
             'ROCK AND STONE! oh... wait',
             'for democracy!!!',
             'say hello to DEMOCRACY',
             'lmao helldivers? me when i dive into my gf\'s pussy'
         ]],
-        ['circus', [
+        [new RegExp('circus', 'i'), [
             'im still sad about gummigoo =(',
             'pomni pomni pomni pomni pomni',
             'are you ready to go on an ADVENTURE?',
             '\*sad TADC outro music plays\*',
             'no exit no exit no exit no exit no exit no exit no exit no exit no exit no exit no exit no exit no exit no exit no exit no exit'
         ]],
-        ['outer wilds', [
+        [new RegExp('outer wild', 'i'), [
             'HOLY SHIT I LOVE OUTER WILDS',
             '||[OUTER WILDS SPOILER]||',
             'https://store.steampowered.com/app/753640/Outer_Wilds/',
-            'you should play Outer Wilds NOW!!!'
+            'you should play Outer Wilds NOW!!!',
+            'https://tenor.com/view/he-played-outer-wilds-outer-wilds-femboy-gif-22516043'
         ]],
-        ['balls', [
-            'gargle my fat balls',
+        [new RegExp('balls', 'i'), [
+            'gargle my fat balls x3',
             'omg balls i know who loves balls. hey <@701201633199128653> look! balls!',
             'ummm did you mean moderators??? oh wait, whats that? nvm they are called balls',
             'https://tenor.com/view/liquid-morkite-deep-rock-galactic-drg-dip-my-balls-balls-gif-3684653839745413089'
         ]],
-        ['men', [
-            'I FUCKING LOVE MEN'
+        [new RegExp('(?:^|\\W)men(?:$|\\W)', 'i'), [
+            'I FUCKING LOVE MEN',
+            'https://tenor.com/view/boykisser-kiss-kiss-gif-kissing-smooch-gif-14255811052713401590',
+            'https://tenor.com/view/mauzymice-boykisser-men-where-are-all-the-men-caption-gif-15918826009865250310'
         ]],
-        ['jackbox', [
-            'Looking for people to join again?'
+        [new RegExp('jackbox', 'i'), [
+            'Looking for people to join again?',
+            'https://tenor.com/view/reverselag-jackbox-ersel-dance-gif-24504431'
         ]]
     ]),
 };
