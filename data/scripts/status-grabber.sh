f43bde583349d3d30f8573b70cb8e825905d8931
rae <raej0000@gmail.com>
2025-02-07 15:52:23 -0800

    public ip update

--- data/scripts/status-grabber.sh
+++ data/scripts/status-grabber.sh
@@ -6,9 +6,9 @@ DOCKER=$FILEPATH"tmp-dockerstatus.txt"
 TUNNEL=$FILEPATH"tmp-tunnelstatus.txt"
 TNL_FORMAT=$FILEPATH"tmp-tunnel-format.txt"
 TNL_FORMATTED=$FILEPATH"tmp-tunnel-formatted.txt"
-#PUBLIC=$FILEPATH"tmp-publicstatus.txt"
-#PB_FORMAT=$FILEPATH"tmp-public-format.txt"
-#PB_FORMATTED=$FILEPATH"tmp-public-formatted.txt"
+PUBLIC=$FILEPATH"tmp-publicstatus.txt"
+PB_FORMAT=$FILEPATH"tmp-public-format.txt"
+PB_FORMATTED=$FILEPATH"tmp-public-formatted.txt"
 EXPORT_JSON=$FILEPATH"tmp-data.json"
 EXPORT_JSON_FRM=$FILEPATH"tmp-formatted.json"
 
@@ -34,23 +34,23 @@ cat $TUNNEL | grep "rtt" | awk -F '/' 'END {print $7}' >> $TNL_FORMAT
 
 sed ':label1 ; N ; $! b label1 ; s/\n//g' $TNL_FORMAT > $TNL_FORMATTED
 
-### grab ping data from public server ip
-#ping -c 8 98.247.215.114 > ${PUBLIC}
-#
-### yay formatting time
-#cat $PUBLIC | grep "rtt" | awk -F '/' 'END {print $5}' > $PB_FORMAT
-#echo " ± " >> $PB_FORMAT
-#cat $PUBLIC | grep "rtt" | awk -F '/' 'END {print $7}' >> $PB_FORMAT
-#
-#sed ':label1 ; N ; $! b label1 ; s/\n//g' $PB_FORMAT > $PB_FORMATTED
+## grab ping data from public server ip
+ping -c 8 71.231.122.199 > ${PUBLIC}
+
+## yay formatting time
+cat $PUBLIC | grep "rtt" | awk -F '/' 'END {print $5}' > $PB_FORMAT
+echo " ± " >> $PB_FORMAT
+cat $PUBLIC | grep "rtt" | awk -F '/' 'END {print $7}' >> $PB_FORMAT
+
+sed ':label1 ; N ; $! b label1 ; s/\n//g' $PB_FORMAT > $PB_FORMATTED
 
 ## oh golly json format time
 echo "{\"dockerStat\":\"" > $EXPORT_JSON
 cat $DOCKER >> $EXPORT_JSON
 echo "\",\"tunnelStat\":\"" >> $EXPORT_JSON
 cat $TNL_FORMATTED >> $EXPORT_JSON
-#echo "\",\"publicStat\":\"" >> $EXPORT_JSON
-#cat $PB_FORMATTED >> $EXPORT_JSON
+echo "\",\"publicStat\":\"" >> $EXPORT_JSON
+cat $PB_FORMATTED >> $EXPORT_JSON
 echo "\"}" >> $EXPORT_JSON
 
 sed ':label1 ; N ; $! b label1 ; s/\n//g' $EXPORT_JSON > $EXPORT_JSON_FRM
