9bb3ceb45dca9ca692cb5030c432ac4a9d651e45
Rae Johnston <73916067+Tors-0@users.noreply.github.com>
2025-11-11 11:01:22 -0800

    remove unnecessary sudo call

--- data/scripts/status-grabber.sh
+++ data/scripts/status-grabber.sh
@@ -22,7 +22,7 @@ touch ${EXPORT_JSON}
 touch ${EXPORT_JSON_FRM}
 
 ## grab status of mc docker container and write to file
-sudo docker ps --filter name=mc --format {{.Status}} > ${DOCKER}
+docker ps --filter name=mc --format {{.Status}} > ${DOCKER}
 
 ## grab ping data from tunnel server
 ping -c 8 10.100.0.2 > ${TUNNEL}
